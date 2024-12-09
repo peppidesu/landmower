2f9a53ac95d5c780fa4c8c6a019f2d539d47d02a
peppidesu <bakker.pepijn@gmail.com>
2024-12-10 00:48:29 +0100

    it worky

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-use std::{collections::HashMap, ops::Add, sync::Arc, time::{Duration, SystemTime}};
-use axum::{extract::State, http::StatusCode, response::Redirect, routing, Json, Router};
-use concurrent_queue::ConcurrentQueue;
+use std::{
+    collections::{hash_map, HashMap}, 
+    fmt::Display, 
+    hash::{Hash, Hasher}, 
+    path::{Path, PathBuf}, 
+    sync::Arc, 
+    time::Duration
+};
+use axum::{
+    extract::State, 
+    http::StatusCode, 
+    response::Redirect, 
+    routing, 
+    Json, 
+    Router
+};
+
 use serde::{Serialize, Deserialize};
 use tokio::sync::RwLock;
+use base64::prelude::*;
+use concurrent_queue::ConcurrentQueue;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Entry {
@@ -24,32 +40,56 @@ pub struct LinkAccessEvent {
 }
 
 #[derive(Clone)]
-pub struct Links(HashMap<String, Entry>);
+pub struct Links { 
+    forward_map: HashMap<String, Entry>, 
+    reverse_map: HashMap<String, Vec<String>>,
+}
 
 pub type HttpError = (StatusCode, String);
 
 impl Links {
-    pub fn load() -> Self {
-        let data = std::fs::read_to_string("data.toml").unwrap();
-        let map: HashMap<_, _> = toml::from_str(&data).unwrap();
-        let map = map.into_iter().collect();
-        Self(map)
+    pub fn load(path: impl AsRef<Path>) -> Result<Self, String> {        
+        let path = path.as_ref();
+
+        if !path.exists() {
+            // Create the directory if it doesn't exist.
+            std::fs::create_dir_all(
+                path.parent()
+                    .ok_or(format!("Invalid link data path: '{}'", path.display()))?
+            ).map_err(|e| format!("Could not create directory: {e}"))?;
+
+            let result: Self = Self { 
+                forward_map: HashMap::new(), 
+                reverse_map: HashMap::new() 
+            };
+
+            result.save(path)?;
+            Ok(result)
+        } else {
+            let data = std::fs::read_to_string(path)
+            .map_err(|e| format!("Could not load links: {e}"))?;
+
+            let forward_map: HashMap<String, Entry> = toml::from_str(&data).unwrap();
+            let mut reverse_map: HashMap<String, Vec<String>> = HashMap::new();
+            for (k, v) in &forward_map {
+                if reverse_map.contains_key(&v.link) {
+                    reverse_map.get_mut(&v.link).unwrap().push(k.clone());
+                } else {
+                    reverse_map.insert(v.link.clone(), vec![k.clone()]);
+                }
+            }
+            Ok(Self { forward_map, reverse_map })
+        }
     }
 
-    pub fn add(&mut self, link: String) -> String {
-        let key = self.generate_key();
-        let entry = Entry {
-            link,
-            metadata: EntryMetadata {
-                used: 0,
-                last_used: std::time::SystemTime::now(),
-                created: std::time::SystemTime::now()
-            }
-        };
-        self.0.insert(key.clone(), entry);
-        key
+    pub fn add(&mut self, link: String) -> Result<Entry, String> {
+        let key = self.generate_key(&link)
+            .ok_or("Link already exists".to_string())?;
+
+        self.add_named(key, link)
     }
-    pub fn add_named(&mut self, key: String, link: String) -> Option<Entry>{
+
+    pub fn add_named(&mut self, key: String, link: String) -> Result<Entry, String> {
         let entry = Entry {
             link,
             metadata: EntryMetadata {
@@ -58,41 +98,101 @@ impl Links {
                 created: std::time::SystemTime::now()
             }
         };
-        self.0.insert(key, entry)
+
+        // Update reverse lookup hashmap
+        match self.reverse_map.entry(entry.link.clone()) {
+            hash_map::Entry::Occupied(mut e) => { 
+                e.get_mut().push(key.clone()); 
+            },
+            hash_map::Entry::Vacant(e) => { 
+                e.insert(vec![key.clone()]); 
+            },
+        }
+
+        // Update main hashmap
+        if let hash_map::Entry::Vacant(e) = self.forward_map.entry(key) {
+            e.insert(entry.clone());
+            Ok(entry)
+        } else {
+            Err("Already exists!".into())
+        }
     }
 
     pub fn get(&self, key: &str) -> Option<&Entry> {
-        self.0.get(key)
+        self.forward_map.get(key)
     }
     
     pub fn get_mut(&mut self, key: &str) -> Option<&mut Entry> {
-        self.0.get_mut(key)
+        self.forward_map.get_mut(key)
     }
 
-    pub fn generate_key(&self) -> String {
-        todo!()
+    pub fn generate_key(&self, link: &str) -> Option<String> {
+        // hash + base64 encode
+        let mut hasher = std::hash::DefaultHasher::new();
+        link.hash(&mut hasher);
+        let hash = BASE64_URL_SAFE_NO_PAD.encode(hasher.finish().to_le_bytes());
+
+        // take first 4 characters, keep adding if there is a collision
+        for i in 4..hash.len() {
+            let key = &hash[..i];
+            if self.forward_map.contains_key(key) { continue; }
+            return Some(key.into());
+        }
+        None
     }
 
-    pub fn save(&self) {
-        let data = toml::to_string(&self.0.iter().collect::<HashMap<_, _>>()).unwrap();
-        std::fs::write("data.toml", data).unwrap();
+    pub fn remove(&mut self, key: &str) -> Option<Entry> {
+        let entry = self.forward_map.remove(key);
+        if let Some(e) = entry {
+            let reverse = self.reverse_map.get_mut(&e.link)
+                .expect("Missing reverse lookup entry (invalid state)");
+            let idx = reverse.iter().position(|x| *x == e.link)
+                .expect("Missing reverse lookup entry (invalid state)");
+            reverse.remove(idx);
+            Some(e)
+        } else {
+            None
+        }
     }
-}
 
-pub struct Config {
-    url_base: String,
-}
-impl Config {
-    pub fn from_env() -> Self {
-        let url_base = std::env::var("URL_BASE").unwrap_or_else(
-            |_| "http://localhost:8080/".to_string()
-        );
-        Self { url_base }
+    pub fn find_by_link(&self, link: impl AsRef<str>) -> Option<&[String]> {
+        self.reverse_map.get(link.as_ref()).map(|v| v.as_slice())
+    }
+
+    pub fn save(&self, path: impl AsRef<Path>) -> Result<(), String>{
+        let path = path.as_ref();
+        let data = toml::to_string(&self.forward_map.iter().collect::<HashMap<_, _>>())
+            .unwrap();
+        std::fs::write("data.toml", data)
+            .map_err(|e| format!("Could not write to file '{}': {}", path.display(), e))?;
+        Ok(())
     }
 }
 
 #[derive(Clone)]
+pub struct Config {
+    link_data_path: PathBuf,
+}
+
+impl Config {
+    pub fn from_env() -> Self {     
+        let link_data_path = std::env::var("LANDMOWER_LINK_DATA_PATH")
+            .map(|s| s.into())
+            .unwrap_or_else(|_| default_link_data_path());
+
+        Self { link_data_path }
+    }
+}
+
+fn default_link_data_path() -> PathBuf {
+    let mut result = dirs::data_local_dir().unwrap();
+    result.push("landmower/links.toml");
+    result
+}
+
+#[derive(Clone)]
 struct AppState {
+    config: Arc<Config>,
     links: Arc<RwLock<Links>>,
     access_event_queue: Arc<ConcurrentQueue<LinkAccessEvent>>
 }
@@ -123,25 +223,37 @@ struct RequestLinkData {
 
 async fn get_links(
     State(state): State<AppState>
-) -> Json<HashMap<String, Entry>> {
+) -> Json<Vec<(String, Entry)>> {
     let links = state.links.read().await;
-    Json(links.0.clone())
+    let res = links.forward_map.clone().into_iter().collect::<Vec<_>>();
+    Json(res)
+}
+
+fn server_error<E: Display>(e: E, msg: impl AsRef<str>) -> HttpError {
+    let msg = msg.as_ref();
+    tracing::error!("{e}");
+    (
+        StatusCode::INTERNAL_SERVER_ERROR, 
+        format!("{msg}\nSee logs for more info.")
+    )
 }
 
 #[axum::debug_handler]
 async fn add_link(
     State(state): State<AppState>,
     Json(req): Json<RequestLinkData>,
-) -> Result<Json<String>, HttpError> {
+) -> Result<Json<Entry>, HttpError> {
     let mut links = state.links.write().await;
-    let key = match req.key {
-        Some(key) => key,
-        None => links.generate_key()
-    };
-    links.add_named(key.clone(), req.link)
-        .ok_or((StatusCode::BAD_REQUEST, "Link already exists".to_string()))?;
-    links.save();
-    Ok(Json(key))
+        
+    let entry = match req.key {
+        Some(key) => links.add_named(key, req.link),
+        None => links.add(req.link)
+    }.map_err(|e| (StatusCode::BAD_REQUEST, e))?;
+    
+    links.save(&state.config.link_data_path)
+        .map_err(|e| server_error(e, "Could not create link: IO error"))?;
+
+    Ok(Json(entry))
 }
 
 async fn get_link(
@@ -154,37 +266,12 @@ async fn get_link(
     Ok(Json(link.clone()))
 }
 
-async fn update_link(
-    State(state): State<AppState>,
-    key: axum::extract::Path<String>,
-    Json(RequestLinkData { key: new_key, link }): Json<RequestLinkData>
-) -> Result<Json<Entry>, HttpError> {
-    let mut links = state.links.write().await;
-    if let Some(new_key) = new_key {
-        let entry = links.0.remove(key.as_str())
-            .ok_or((StatusCode::NOT_FOUND, "Link not found".to_string()))?;
-        links.0.insert(new_key, entry.clone())
-            .ok_or((StatusCode::BAD_REQUEST, "Link already exists".to_string()))?;
-
-        Ok(Json(entry))
-    }
-    else {
-        
-        let entry = links.get_mut(&key)
-            .ok_or((StatusCode::NOT_FOUND, "Link not found".to_string()))?;
-        entry.link = link;
-
-        Ok(Json(entry.clone()))
-    }
-    
-}
-
 async fn delete_link(
     State(state): State<AppState>,
     key: axum::extract::Path<String>
 ) -> Result<Json<String>, HttpError> {
     let mut links = state.links.write().await;
-    links.0.remove(key.as_str())
+    links.remove(key.as_str())
         .ok_or((StatusCode::NOT_FOUND, "Link not found".to_string()))?;
     Ok(Json("ok".to_string()))
 }
@@ -208,17 +295,26 @@ async fn main() {
     tracing_subscriber::fmt::init();
     
     let state = AppState { 
-        links: Arc::new(RwLock::new(Links::load())), 
-        access_event_queue: Arc::new(ConcurrentQueue::unbounded()) 
+        config: Config::from_env().into(),
+        links: RwLock::new(Links::load("data.toml").unwrap()).into(), 
+        access_event_queue: ConcurrentQueue::unbounded().into()
     };
 
     let app = Router::new()
-        .route("/s/:key", routing::get(redirect))
-        .route("/api/links", routing::get(get_links))
-        .route("/api/links", routing::post(add_link))
-        .route("/api/links/:key", routing::get(get_link))
-        .route("/api/links/:key", routing::post(update_link))
-        .route("/api/links/:key", routing::delete(delete_link))
+        .route(
+            "/s/:key", 
+            routing::get(redirect)
+        )
+        .route(
+            "/api/links", 
+            routing::get(get_links)
+                    .post(add_link)
+        )
+        .route(
+            "/api/links/:key", 
+            routing::get(get_link)
+                    .delete(delete_link)
+        )        
         .with_state(state.clone());
     
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
