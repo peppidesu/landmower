85a79dde1ba353e019fa69428ddccd94978b2eaf
peppidesu <bakker.pepijn@gmail.com>
2024-12-10 01:20:29 +0100

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -39,15 +39,22 @@ pub struct LinkAccessEvent {
     timestamp: std::time::SystemTime,
 }
 
+/// Stores alias->link mappings and the reverse mapping.
 #[derive(Clone)]
 pub struct Links { 
+    /// Forward hashmap is used for finding the associated link for a given alias.
     forward_map: HashMap<String, Entry>, 
+    /// Inverse of the forward hashmap.
+    /// The forward mapping is surjective, so each link can have multiple associated aliases.
+    /// 
+    /// Note: might be worth benching to see if linear search is actually slower.
     reverse_map: HashMap<String, Vec<String>>,
 }
 
 pub type HttpError = (StatusCode, String);
 
 impl Links {
+    /// Load link data from the given file
     pub fn load(path: impl AsRef<Path>) -> Result<Self, String> {        
         let path = path.as_ref();
 
@@ -57,64 +64,33 @@ impl Links {
                 path.parent()
                     .ok_or(format!("Invalid link data path: '{}'", path.display()))?
             ).map_err(|e| format!("Could not create directory: {e}"))?;
-
+            
+            // Create empty link storage & write to file
             let result: Self = Self { 
                 forward_map: HashMap::new(), 
                 reverse_map: HashMap::new() 
             };
-
             result.save(path)?;
             Ok(result)
         } else {
+            // Read file contents
             let data = std::fs::read_to_string(path)
-            .map_err(|e| format!("Could not load links: {e}"))?;
+                .map_err(|e| format!("Could not load links: {e}"))?;
 
             let forward_map: HashMap<String, Entry> = toml::from_str(&data).unwrap();
+
+            // Build reverse lookup
             let mut reverse_map: HashMap<String, Vec<String>> = HashMap::new();
             for (k, v) in &forward_map {
                 if reverse_map.contains_key(&v.link) {
+                    // link already has associated key; add to existing list
                     reverse_map.get_mut(&v.link).unwrap().push(k.clone());
                 } else {
+                    // create a new entry for this link
                     reverse_map.insert(v.link.clone(), vec![k.clone()]);
                 }
             }
             Ok(Self { forward_map, reverse_map })
-        }
-    }
-
-    pub fn add(&mut self, link: String) -> Result<Entry, String> {
-        let key = self.generate_key(&link)
-            .ok_or("Link already exists".to_string())?;
-
-        self.add_named(key, link)
-    }
-
-    pub fn add_named(&mut self, key: String, link: String) -> Result<Entry, String> {
-        let entry = Entry {
-            link,
-            metadata: EntryMetadata {
-                used: 0,
-                last_used: std::time::SystemTime::now(),
-                created: std::time::SystemTime::now()
-            }
-        };
-
-        // Update reverse lookup hashmap
-        match self.reverse_map.entry(entry.link.clone()) {
-            hash_map::Entry::Occupied(mut e) => { 
-                e.get_mut().push(key.clone()); 
-            },
-            hash_map::Entry::Vacant(e) => { 
-                e.insert(vec![key.clone()]); 
-            },
-        }
-
-        // Update main hashmap
-        if let hash_map::Entry::Vacant(e) = self.forward_map.entry(key) {
-            e.insert(entry.clone());
-            Ok(entry)
-        } else {
-            Err("Already exists!".into())
         }
     }
 
@@ -126,23 +102,78 @@ impl Links {
         self.forward_map.get_mut(key)
     }
 
-    pub fn generate_key(&self, link: &str) -> Option<String> {
+    /// Insert a new mapping with a generated key and the given link.
+    ///
+    /// ## Errors
+    ///
+    /// This function will return an error if the key is already in use, a.k.a. the link
+    /// already has an associated mapping 
+    pub fn add(&mut self, link: String) -> Result<Entry, String> {
+        let key = self.generate_key(&link)        
+            .ok_or("Link already has an associated alias.".to_string())?;
+        self.add_named(key, link)
+    }
+    
+    fn generate_key(&self, link: &str) -> Option<String> {
         // hash + base64 encode
         let mut hasher = std::hash::DefaultHasher::new();
         link.hash(&mut hasher);
         let hash = BASE64_URL_SAFE_NO_PAD.encode(hasher.finish().to_le_bytes());
 
         // take first 4 characters, keep adding if there is a collision
-        for i in 4..hash.len() {
+        for i in 4..=hash.len() {
             let key = &hash[..i];
-            if self.forward_map.contains_key(key) { continue; }
+            if let Some(other) = self.forward_map.get(key) { 
+                if other.link == link {
+                    return None;
+                }                
+                continue;
+            }
             return Some(key.into());
         }
-        None
+
+        None // hash collision -> link already present in storage
     }
 
+    /// Insert a new mapping with the given key and link.
+    ///
+    /// ## Errors
+    ///
+    /// This function will return an error if the given key is already in use.
+    pub fn add_named(&mut self, key: String, link: String) -> Result<Entry, String> {
+        let entry = Entry {
+            link,
+            metadata: EntryMetadata {
+                used: 0,
+                last_used: std::time::SystemTime::now(),
+                created: std::time::SystemTime::now()
+            }
+        };
+        // Update reverse hashmap
+        match self.reverse_map.entry(entry.link.clone()) {
+            hash_map::Entry::Occupied(mut e) => { 
+                e.get_mut().push(key.clone()); 
+            },
+            hash_map::Entry::Vacant(e) => { 
+                e.insert(vec![key.clone()]); 
+            },
+        }
+        // Update forward hashmap
+        if let hash_map::Entry::Vacant(e) = self.forward_map.entry(key) {
+            e.insert(entry.clone());
+            Ok(entry)
+        } else {
+            Err("Key already in use.".into())
+        }
+    }
+
+    /// Remove the given mapping.
+    /// 
+    /// Returns `None` if the link alias does not exist.
     pub fn remove(&mut self, key: &str) -> Option<Entry> {
         let entry = self.forward_map.remove(key);
+        
+        // Update reverse hashmap
         if let Some(e) = entry {
             let reverse = self.reverse_map.get_mut(&e.link)
                 .expect("Missing reverse lookup entry (invalid state)");
@@ -155,10 +186,14 @@ impl Links {
         }
     }
 
+    /// Find aliases that map to the given link.
+    /// 
+    /// Returns `None` if the link has no associated aliases.
     pub fn find_by_link(&self, link: impl AsRef<str>) -> Option<&[String]> {
         self.reverse_map.get(link.as_ref()).map(|v| v.as_slice())
     }
 
+    /// Save link data to the given file.
     pub fn save(&self, path: impl AsRef<Path>) -> Result<(), String>{
         let path = path.as_ref();
         let data = toml::to_string(&self.forward_map.iter().collect::<HashMap<_, _>>())
